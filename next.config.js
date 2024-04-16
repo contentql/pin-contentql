e4529c48dc5a896a7869b2a8a1f5d1a85eb5388d
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-04-16 11:21:19 +0530

    chore: updated nextconfig for images

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'payload-in-next-production.up.railway.app', //TODO: update accordingly
+        hostname: 'contentql.io',
       },
       {
         protocol: 'https',
