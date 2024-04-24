202d4c24187740819163d33b5144efda0c054c3a
charanm927 <charanm927@gmail.com>
2024-04-24 15:23:10 +0530

    chore: trpc added for individual layout

--- src/app/(marketing)/(non-protected)/[[...slug]]/page.tsx
+++ src/app/(marketing)/(non-protected)/[[...slug]]/page.tsx
@@ -1,12 +1,10 @@
-import { getPayloadClient } from '@/get-payload';
-import RendorBlocks from '@/utils/RendorBlocks';
+import { getPayloadClient } from '@/get-payload'
+import RendorBlocks from '@/utils/RendorBlocks'
 
-
-const Page = async ({ params }: { params: { slug: string[]} }) => {
+const Page = async ({ params }: { params: { slug: string[] } }) => {
   // const { slug } = params
 
   const slug = params.slug?.at(0) || 'index'
-
 
   const payload = await getPayloadClient()
 
@@ -21,20 +19,23 @@ const Page = async ({ params }: { params: { slug: string[]} }) => {
 
   return (
     <div>
-      <RendorBlocks layout={pageData[0]?.layout} />
+      <RendorBlocks layout={pageData[0]?.layout} slug={slug} />
     </div>
   )
 }
 
 export const generateStaticParams = async () => {
   const payload = await getPayloadClient()
-  const { docs: pageData } = await payload.find({ collection: 'pages' })
+  const { docs: pageData } = await payload.find({
+    collection: 'pages',
+    pagination: false,
+  })
 
   const arrayOfPageSlugs = pageData?.map(page => {
     return page.slug
   })
 
-  return [...arrayOfPageSlugs]
+  return arrayOfPageSlugs
 }
 
 export default Page
