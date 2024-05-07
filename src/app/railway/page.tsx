ccce0645ec00deca9606041842869e9d880d22d5
ManikantaPotnuru9176 <manikantapotnur9176@gmail.com>
2024-05-07 16:27:48 +0530

    chore: created a railway page

---
+++ src/app/railway/page.tsx
@@ -0,0 +1,18 @@
+const railway = () => {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        paddingTop: '100px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <h3>Click the below button create a project and required services.</h3>
+      <button type='button'>Create New</button>
+    </div>
+  )
+}
+
+export default railway
